2a3d4cb9fa24a41298a1793580b66458b7dfccdb
Kevin Hawthorne <138548850+Kefir-png@users.noreply.github.com>
2024-09-25 16:45:35 -0400

    Add proffession variable options. Add bool variables. Add to player introduction text. Add you explode.

--- Hawthorne-Kevin-a1-InteractiveTerminalProject/Program.cs
+++ Hawthorne-Kevin-a1-InteractiveTerminalProject/Program.cs
@@ -1,6 +1,11 @@
 ﻿
-//setting
+using System;
 using System.Net.Security;
+
+//setiing bools
+bool isAlc = false;
+bool isDiv = false;
+bool isEnc = false;
 
 Console.WriteLine("You are a Wizards Apprentis");
 
@@ -9,22 +14,36 @@ Console.WriteLine("What is your name?");
 String PlayerName = Console.ReadLine();
 
 //asking for player proffession and list proffession options
+Console.WriteLine("hello, " + PlayerName);
 Console.WriteLine("are you a...");
 Console.WriteLine("?Alchemist? (s) apprentis \n?Diviner? (s) apprentis \n?Enchanter? (s) apprentis");
-
-string Alchemist = "Alchemist";
-string Diviner = "Diviner";
-string Enchanter = "Enchanter";
 string Proffession = Console.ReadLine();
-{ 
-if (Proffession.Contains(Alchemist) == true)
-      
-        ;
-} 
 
-//improper input 
+//input results
 
-//setting using player information
+if (Proffession == "Alchemist")
+{
+    Console.WriteLine("u r alc");
+    isAlc = true;
+
+}
+else if (Proffession == "Diviner")
+{
+    Console.WriteLine("u r div");
+    isDiv = true;
+
+}
+else if (Proffession == "Enchanter")
+{
+    Console.WriteLine("u r ench");
+    isEnc = true;
+}
+else
+{
+    Console.WriteLine("you esplode");
+}
+
+/*/setting using player information
 Console.Write(PlayerName);
 Console.Write(" is a ");
 Console.Write(Proffession);
@@ -33,8 +52,25 @@ Console.Write(PlayerName);
 Console.Write(" is running late to their apprentiship. ");
 Console.Write(PlayerName);
 Console.WriteLine(" should get going... \n?leave? apartment");
+*/
 //request player input to progress
-
+Console.ReadLine();
+{
+    if (Console.ReadLine() == "leave")
+    {
+        Console.WriteLine(PlayerName + "aprouches their apartments front door. On the wood of the door a magical glyph is written. The glyph says...");
+        Console.WriteLine("...");
+        Console.WriteLine("Im two faces; both a buigining and an end.");
+        Console.WriteLine("When one closes the other one opens. Though i am no map, I sometimes need a key.");
+        Console.WriteLine("The more of me you open the more you will be free.");
+        Console.WriteLine("...");
+        Console.WriteLine("What am i?");
+    }
+    else
+    {
+        Console.WriteLine("you esplode");
+    }
+}
 //inproper input 
 
 //proper input progresses to riddle

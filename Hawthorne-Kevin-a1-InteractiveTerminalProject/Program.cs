66e7f8a660ef78242dc2891d9e4a3b4074b0e73e
Kevin Hawthorne <138548850+Kefir-png@users.noreply.github.com>
2024-09-25 05:07:13 -0400

    Add basic coding framework for first half of project.

--- Hawthorne-Kevin-a1-InteractiveTerminalProject/Program.cs
+++ Hawthorne-Kevin-a1-InteractiveTerminalProject/Program.cs
@@ -1,16 +1,38 @@
 ﻿
 //setting
+using System.Net.Security;
+
+Console.WriteLine("You are a Wizards Apprentis");
 
 //requesting player input for name
+Console.WriteLine("What is your name?");
+String PlayerName = Console.ReadLine();
 
-//asking for player proffession 
+//asking for player proffession and list proffession options
+Console.WriteLine("are you a...");
+Console.WriteLine("?Alchemist? (s) apprentis \n?Diviner? (s) apprentis \n?Enchanter? (s) apprentis");
 
-//list proffession options
+string Alchemist = "Alchemist";
+string Diviner = "Diviner";
+string Enchanter = "Enchanter";
+string Proffession = Console.ReadLine();
+{ 
+if (Proffession.Contains(Alchemist) == true)
+      
+        ;
+} 
 
-//improper input
+//improper input 
 
 //setting using player information
-
+Console.Write(PlayerName);
+Console.Write(" is a ");
+Console.Write(Proffession);
+Console.Write("'s apprentis. ");
+Console.Write(PlayerName);
+Console.Write(" is running late to their apprentiship. ");
+Console.Write(PlayerName);
+Console.WriteLine(" should get going... \n?leave? apartment");
 //request player input to progress
 
 //inproper input 

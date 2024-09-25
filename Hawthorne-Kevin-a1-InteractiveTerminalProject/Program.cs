d1f344e856fe56b5003e843a4a1856521886c248
Kevin Hawthorne <138548850+Kefir-png@users.noreply.github.com>
2024-09-25 02:05:44 -0400

    Add notes for code.

---
+++ Hawthorne-Kevin-a1-InteractiveTerminalProject/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//setting
+
+//requesting player input for name
+
+//asking for player proffession 
+
+//list proffession options
+
+//improper input
+
+//setting using player information
+
+//request player input to progress
+
+//inproper input 
+
+//proper input progresses to riddle
+
+//proper input
+
+//inproper riddle input consequnses 
